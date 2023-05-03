267c46a549e8fb44c8b3bbd5a060672d41fa8539
hajime <impapa68@gmail.com>
2023-05-03 20:06:09 +0900

    [Feat/#3] Audio Visualizer 애니메이션 개선

--- Pressor/Pressor/Views/RecordViews/AudioVisualizerView.swift
+++ Pressor/Pressor/Views/RecordViews/AudioVisualizerView.swift
@@ -11,25 +11,23 @@ import Charts
 
 struct AudioVisualizerView: View {
     @StateObject private var audioInputManager = AudioInputManager()
-    @State private var barHeights: [CGFloat] = Array(repeating: 0, count: 50)
+    @State private var barHeights: [CGFloat] = Array(repeating: 0, count: 80)
 
     var body: some View {
         VStack {
-            HStack(spacing: 4) {
-                // 각 막대를 반복하여 표시
-                ForEach(0..<barHeights.count) { index in
-                    BarView(height: barHeights[index])
+            ZStack {
+                HStack {
+                    // 왼쪽 막대 그룹
+                    HStack(spacing: 2) {
+                        ForEach(0..<barHeights.count) { index in
+                            BarView(height: barHeights[index] * weight(for: index))
+                                .animation(.easeInOut(duration: 0.15), value: barHeights[index])
+                        }
+                    }
                 }
-            }
-
-//            Button(action: toggleRecording) {
-//                Text("Toggle Recording")
-//                    .padding()
-//                    .background(Color.blue)
-//                    .foregroundColor(.white)
-//                    .cornerRadius(10)
-//            }
-        }
+            }}
+        .frame(height: 80)
+        .background(.black)
         .onAppear {
             // 뷰가 나타날 때 녹음 시작
             audioInputManager.startRecording { buffer in
@@ -40,51 +38,59 @@ struct AudioVisualizerView: View {
         }
     }
 
-//    private func toggleRecording() {
-//        if audioInputManager.audioEngine.isRunning {
-//            audioInputManager.stopRecording()
-//        } else {
-//            audioInputManager.startRecording { buffer in
-//                DispatchQueue.main.async {
-//                    self.updateBarHeights(with: buffer)
-//                }
-//            }
-//        }
-//    }
-
-    // 막대 높이 업데이트(에니베이션)
+    // 인덱스에 따른 가중치 계산
+    private func weight(for index: Int) -> CGFloat {
+        let halfCount = CGFloat(barHeights.count / 2)
+        let relativeIndex = abs(CGFloat(index) - halfCount)
+        let weight = 1 - (2 * relativeIndex / halfCount)
+        return weight
+    }
+    
+    //    private func toggleRecording() {
+    //        if audioInputManager.audioEngine.isRunning {
+    //            audioInputManager.stopRecording()
+    //        } else {
+    //            audioInputManager.startRecording { buffer in
+    //                DispatchQueue.main.async {
+    //                    self.updateBarHeights(with: buffer)
+    //                }
+    //            }
+    //        }
+    //    }
+    
+    // 막대 높이 업데이트(에니메이션)
     private func updateBarHeights(with buffer: AVAudioPCMBuffer) {
         let samples = Array(UnsafeBufferPointer(start: buffer.floatChannelData?[0], count: Int(buffer.frameLength)))
         let sampleCount = samples.count
         let blockSize = sampleCount / barHeights.count
-
+        
         for i in 0..<barHeights.count {
             let start = i * blockSize
             let end = start + blockSize
             let slice = samples[start..<end]
-
+            
             let rms = calculateRMS(for: Array(slice))
             let normalizedHeight = CGFloat(min(max(0, rms), 1)) * UIScreen.main.bounds.height / 3
             barHeights[i] = normalizedHeight
         }
     }
-
+    
     // RMS 계산
     private func calculateRMS(for samples: [Float]) -> Float {
         let sumOfSquares = samples.reduce(0) { $0 + $1 * $1 }
         let rms = sqrt(sumOfSquares / Float(samples.count))
         return rms
     }
-
+    
 }
 
 
 struct BarView: View {
     var height: CGFloat
-
+    
     var body: some View {
         RoundedRectangle(cornerRadius: 4)
-            .fill(Color.black)
+            .fill(Color.white)
             .frame(height: height)
     }
 }
@@ -92,28 +98,28 @@ struct BarView: View {
 class AudioInputManager: ObservableObject {
     var audioEngine: AVAudioEngine
     private var audioBufferHandler: ((AVAudioPCMBuffer) -> Void)?
-
+    
     init() {
         audioEngine = AVAudioEngine()
     }
-
+    
     // 녹음 시작
     func startRecording(_ audioBufferHandler: @escaping (AVAudioPCMBuffer) -> Void) {
         self.audioBufferHandler = audioBufferHandler
         let inputNode = audioEngine.inputNode
         let format = inputNode.outputFormat(forBus: 0)
-
+        
         inputNode.installTap(onBus: 0, bufferSize: 1024, format: format) { buffer, _ in
             self.audioBufferHandler?(buffer)
         }
-
+        
         do {
             try audioEngine.start()
         } catch {
             print("Error starting audio engine: \(error)")
         }
     }
-
+    
     func stopRecording() {
         audioEngine.inputNode.removeTap(onBus: 0)
         audioEngine.stop()
