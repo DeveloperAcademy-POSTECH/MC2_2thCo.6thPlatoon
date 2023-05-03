bb8b9633e4ab66fd0bc6c3531a348a69cfef36e9
hajime <impapa68@gmail.com>
2023-05-04 03:37:17 +0900

    [Feat/#6] InterviewRecordingView 타이머, 화자 전환, 화면 플립

--- Pressor/Pressor/Views/InterviewViews/InterviewRecordingView.swift
+++ Pressor/Pressor/Views/InterviewViews/InterviewRecordingView.swift
@@ -12,17 +12,57 @@ struct InterviewRecordingView: View {
     @State private var isRecording = false
     @State private var isPaused = false
     @State private var duration: TimeInterval = 0.0
-
+    @State private var backgroundColor: Color = SpeakerColor.speakerOne
+    @State private var timer: Timer? = nil
+    @State private var rotationAngle: Double = 0
+    
+    // 화자 구분 색상
+    private struct SpeakerColor {
+           static let speakerOne = Color(red: 1, green: 234 / 255, blue: 132 / 255)
+           static let speakerTwo = Color(red: 177 / 255, green: 232 / 255, blue: 1)
+       }
+    
+    // 타이머 시작 함수
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            duration += 0.1
+        }
+    }
+    
+    // 타이머 중지 함수
+    private func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    // 타이머 초기화 함수
+    private func resetDuration() {
+        duration = 0
+    }
+    
+    // 타이머 일시정지 함수
+    private func pauseResumeTimer() {
+        if timer == nil {
+            startTimer()
+        } else {
+            stopTimer()
+        }
+    }
+    
     var body: some View {
         VStack {
             HStack {
                 Spacer()
                 Button(action: {
                     // 화면 플립 기능
+                    withAnimation(.easeInOut(duration: 0.6)) {
+                        rotationAngle += 180
+                    }
                 }) {
                     Image(systemName: "arrow.up.arrow.down.circle")
                         .resizable()
                         .frame(width: 40, height: 40)
+                        .foregroundColor(.black)
                 }
                 .padding()
             }
@@ -31,6 +71,7 @@ struct InterviewRecordingView: View {
             
             Button(action: {
                 // 화자 전환 기능
+                backgroundColor = backgroundColor == SpeakerColor.speakerOne ? SpeakerColor.speakerTwo : SpeakerColor.speakerOne
             }) {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.blue)
@@ -38,12 +79,16 @@ struct InterviewRecordingView: View {
             }
             
             Spacer()
-            AudioVisualizerView(audioInputManager: audioInputManager, isRecording: $isRecording)
+            // 오디오 비쥬얼라이저 뷰
+            AudioVisualizerView(audioInputManager: audioInputManager, isRecording: $isRecording, isPaused: $isPaused)
             HStack {
+                // 타이머 표시
                 Text(String(format: "%.1fs", duration))
                     .font(.title2)
+                    .frame(minWidth: 50)
                     .padding(.leading)
                 
+                // 녹음 버튼
                 Button(action: {
                     isRecording.toggle()
                     if isRecording {
@@ -52,8 +97,12 @@ struct InterviewRecordingView: View {
                                 // UI 관련 로직
                             }
                         }
+                        startTimer()
                     } else {
                         audioInputManager.stopRecording()
+                        isPaused = false
+                        stopTimer()
+                        resetDuration()
                     }
                 }) {
                     Image(systemName: isRecording ? "stop.circle" : "record.circle")
@@ -64,6 +113,7 @@ struct InterviewRecordingView: View {
                 
                 Button(action: {
                     isPaused.toggle()
+                    pauseResumeTimer()
                     if isPaused {
                         // 녹음 일시정지
                     } else {
@@ -74,10 +124,13 @@ struct InterviewRecordingView: View {
                         .resizable()
                         .frame(width: 60, height: 60)
                 }
+                .disabled(!isRecording)
                 .padding(.trailing)
             }
             .padding(.bottom)
         }
+        .rotation3DEffect(.degrees(rotationAngle), axis: (x: 1.0, y: 0.0, z: 0.0))
+        .background(backgroundColor)
         .onAppear {
             audioInputManager.prepare()
         }
@@ -86,6 +139,7 @@ struct InterviewRecordingView: View {
         }
     }
 }
+
 
 struct InterviewRecordingView_Previews: PreviewProvider {
     static var previews: some View {
