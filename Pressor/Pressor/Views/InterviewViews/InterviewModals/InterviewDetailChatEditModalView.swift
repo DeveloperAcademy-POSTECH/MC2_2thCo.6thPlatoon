f3154caadd1784fa8144f04b2802532d55bc5179
valselee <sollleky72@gmail.com>
2023-05-05 18:15:28 +0900

    [Add] #10 Modal View Files Added

---
+++ Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailChatEditModalView.swift
@@ -0,0 +1,20 @@
+//
+//  InterviewDetailChatEditModalView.swift
+//  Pressor
+//
+//  Created by Celan on 2023/05/04.
+//
+
+import SwiftUI
+
+struct InterviewDetailChatEditModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterviewDetailChatEditModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterviewDetailChatEditModalView()
+    }
+}
---
+++ Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailEditModalView.swift
@@ -0,0 +1,20 @@
+//
+//  InterviewDetailEditModalView.swift
+//  Pressor
+//
+//  Created by Celan on 2023/05/04.
+//
+
+import SwiftUI
+
+struct InterviewDetailEditModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterviewDetailEditModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterviewDetailEditModalView()
+    }
+}
