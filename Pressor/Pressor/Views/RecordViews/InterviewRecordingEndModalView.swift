7c5a66534f6d70f37775caddcebb02c5e1a44e06
hajime <impapa68@gmail.com>
2023-05-08 10:13:22 +0900

    [Feat/#12] InterviewRecordingEndModelView 구현

--- Pressor/Pressor/Views/RecordViews/InterviewRecordingEndModalView.swift
+++ Pressor/Pressor/Views/RecordViews/InterviewRecordingEndModalView.swift
@@ -1,4 +1,3 @@
-//
 //  InterviewRecordingEndModalView.swift
 //  Pressor
 //
@@ -8,13 +7,44 @@
 import SwiftUI
 
 struct InterviewRecordingEndModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var viewModel: InterviewRecordViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                // 인터뷰 제목 섹션
+                Section(header: Text("인터뷰 제목")) {
+                    TextField("새로운 인터뷰", text: $viewModel.interviewTitle)
+                }
+                // 대상자 정보 섹션
+                Section(header: Text("대상자 정보")) {
+                    TextField("이름", text: $viewModel.intervieweeName)
+                    
+                    TextField("이메일", text: $viewModel.email)
+                    
+                    TextField("전화번호", text: $viewModel.phoneNum)
+                }
+            }
+            .navigationBarTitle("정보", displayMode: .inline)
+            .navigationBarItems(leading:
+                                    // 취소 버튼
+                                Button("취소") {
+                presentationMode.wrappedValue.dismiss()
+            }.foregroundColor(.red),
+                                trailing:
+                                    // 완료 버튼
+                                Button("완료") {
+                viewModel.saveInterviewRecord()
+                presentationMode.wrappedValue.dismiss()
+            }.disabled(viewModel.isDoneButtonDisabled)
+            )
+        }
     }
 }
 
 struct InterviewRecordingEndModalView_Previews: PreviewProvider {
     static var previews: some View {
-        InterviewRecordingEndModalView()
+        InterviewRecordingEndModalView(viewModel: InterviewRecordViewModel())
     }
 }
