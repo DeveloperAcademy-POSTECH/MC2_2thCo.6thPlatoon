dc3493e725264a823fb7228fb5794fb496fbf772
Suppppppp <u1316@daum.net>
2023-05-08 15:53:59 +0900

    [add] 기존파일에서 InterviewListView로 이름이 변경됨

---
+++ Pressor/Pressor/Views/InterviewViews/InterviewListView.swift
@@ -0,0 +1,20 @@
+//
+//  InterviewListView.swift
+//  Pressor
+//
+//  Created by sup on 2023/05/03.
+//
+
+import SwiftUI
+
+struct InterviewListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterviewListView_Previews: PreviewProvider {
+    static var previews: some View {
+        InterviewListView()
+    }
+}
