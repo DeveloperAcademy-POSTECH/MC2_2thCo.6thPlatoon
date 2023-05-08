6ff0a8fa7caab7577c9a44b293fd2390ed109f1c
hajime <impapa68@gmail.com>
2023-05-08 10:12:22 +0900

    [Feat/#12] InterviewRecordViewModel 구현

--- Pressor/Pressor/ViewModels/InterviewRecordViewModel.swift
+++ Pressor/Pressor/ViewModels/InterviewRecordViewModel.swift
@@ -1,4 +1,3 @@
-//
 //  InterviewRecordViewModel.swift
 //  Pressor
 //
@@ -6,3 +5,40 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class InterviewRecordViewModel: ObservableObject {
+    @Published var interviewTitle: String = ""
+    @Published var intervieweeName: String = ""
+    @Published var email: String = ""
+    @Published var phoneNum: String = ""
+    
+    // 각 필드의 초기값
+    private let initialInterviewTitle = ""
+    private let initialIntervieweeName = ""
+    private let initialEmail = ""
+    private let initialPhoneNum = ""
+    
+    // 모든 필드가 초기값과 동일한 경우 Done 버튼 비활성화
+    var isDoneButtonDisabled: Bool {
+        interviewTitle == initialInterviewTitle &&
+        intervieweeName == initialIntervieweeName &&
+        email == initialEmail &&
+        phoneNum == initialPhoneNum
+    }
+    
+    // 인터뷰 기록 저장 함수
+    func saveInterviewRecord() {
+        // STT 기록 추가
+        let sttRecords = [InterviewSTTRecord(duration: 0, text: "")]
+        // interviewRecord를 데이터 저장소에 저장
+        let interviewRecord = InterviewRecord(id: UUID(),
+                                              subject: interviewTitle,
+                                              email: email,
+                                              date: Date(),
+                                              phoneNum: phoneNum,
+                                              intervieweeName: intervieweeName,
+                                              interviewSTTRecords: sttRecords)
+    }
+}
