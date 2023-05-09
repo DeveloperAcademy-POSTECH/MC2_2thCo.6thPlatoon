95b703ad496c9b2630fa573db3f45753a97eed65
SangwooKim <mollangcow@gmail.com>
2023-05-09 15:02:41 +0900

    [Feat/#22] 녹음버튼 이미지로 교체, 세부 디자인 수정

--- Pressor/Pressor/Views/RecordViews/MainRecordView.swift
+++ Pressor/Pressor/Views/RecordViews/MainRecordView.swift
@@ -15,45 +15,54 @@ struct MainRecordView: View {
     @State var count = 3
     @State var isShownFullScreenCover = false
     
+    init() {
+      UITabBar.appearance().scrollEdgeAppearance = .init()
+    }
+    
     var body: some View {
         
         TabView {
             NavigationView {
                 VStack {
-                    Text("새로운 인터뷰를 시작하려면\n아래 녹음 버튼을 눌러주세요.")
-                        .foregroundColor(Color(red: 153/255, green: 153/255, blue: 153/255))
+                    Text("새로운 인터뷰 녹음을 시작하려면\n아래 녹음 버튼을 눌러주세요.")
+                        .font(.headline)
+                        .foregroundColor(Color(red: 193/255, green: 193/255, blue: 200/255))
                         .fontWeight(.semibold)
-                        .padding(.bottom, 67)
+                        .multilineTextAlignment(.center)
+                        .lineSpacing(4)
+                        .padding(.bottom, 50)
                     VStack {
                         Button {
                             self.isShownFullScreenCover.toggle()
                         } label: {
-                            VStack {
-                                ZStack {
-                                    Rectangle()
-                                        .frame(width: 216, height: 216)
-                                        .cornerRadius(88)
-                                        .foregroundColor(Color(red: 248/255, green: 248/255, blue: 249/255, opacity: 1))
-                                    Rectangle()
-                                        .frame(width: 182, height: 182)
-                                        .cornerRadius(70)
-                                        .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 1))
-                                        .shadow(color: Color.gray.opacity(0.4), radius: 30, x: 0, y: 0)
-                                    Rectangle()
-                                        .frame(width: 154, height: 154)
-                                        .cornerRadius(57)
-                                        .foregroundColor(Color.white)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 57)
-                                                .strokeBorder(Color.red, lineWidth: 3)
-                                        )
-                                    Image(systemName: "mic.fill")
-                                        .resizable()
-                                        .frame(width: 38, height: 60)
-                                        .foregroundColor(Color.red)
-                                }
-                                .padding(.bottom, 67)
-                            }
+                            Image("mic_button")
+                                .padding(.bottom, 40)
+//                            VStack {
+//                                ZStack {
+//                                    Rectangle()
+//                                        .frame(width: 216, height: 216)
+//                                        .cornerRadius(88)
+//                                        .foregroundColor(Color(red: 248/255, green: 248/255, blue: 249/255, opacity: 1))
+//                                    Rectangle()
+//                                        .frame(width: 182, height: 182)
+//                                        .cornerRadius(70)
+//                                        .foregroundColor(Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 1))
+//                                        .shadow(color: Color.gray.opacity(0.4), radius: 30, x: 0, y: 0)
+//                                    Rectangle()
+//                                        .frame(width: 154, height: 154)
+//                                        .cornerRadius(57)
+//                                        .foregroundColor(Color.white)
+//                                        .overlay(
+//                                            RoundedRectangle(cornerRadius: 57)
+//                                                .strokeBorder(Color.red, lineWidth: 3)
+//                                        )
+//                                    Image(systemName: "mic.fill")
+//                                        .resizable()
+//                                        .frame(width: 38, height: 60)
+//                                        .foregroundColor(Color.red)
+//                                }
+//                                .padding(.bottom, 67)
+//                            }
                         }
                         .fullScreenCover(isPresented: $isShownFullScreenCover) {
                             InterviewRecordingView()
@@ -68,10 +77,11 @@ struct MainRecordView: View {
                                 VStack {
                                     Image(systemName: "note.text")
                                         .resizable()
-                                        .frame(width: 35, height: 33)
+                                        .frame(width: 38, height: 35)
                                         .foregroundColor(Color.accentColor)
                                     Text("대본편집")
                                         .foregroundColor(Color.accentColor)
+                                        .fontWeight(.semibold)
                                 }
                                 .confirmationDialog("타이틀", isPresented: $isSheetShowing) {
                                     Button("대본 삭제", role: .destructive) {
@@ -93,10 +103,12 @@ struct MainRecordView: View {
                                 VStack {
                                     Image(systemName: "note.text.badge.plus")
                                         .resizable()
-                                        .frame(width: 35, height: 33)
+                                        .frame(width: 42, height: 35)
                                         .foregroundColor(Color.accentColor)
+                                        .padding(.leading, 5)
                                     Text("대본추가")
                                         .foregroundColor(Color.accentColor)
+                                        .fontWeight(.semibold)
                                 }
                                 .confirmationDialog("타이틀", isPresented: $isSheetShowing) {
                                     Button("대본 삭제", role: .destructive) {
@@ -115,10 +127,9 @@ struct MainRecordView: View {
                         }
                     }
                 }
-                .navigationBarTitle("Pressor")
                 .navigationBarItems(trailing: NavigationLink(destination: AddRecordScriptModalView()) {
                     Image(systemName: "gearshape.fill")})
-                .foregroundColor(Color(red: 209/255, green: 209/255, blue: 214/255))
+                .foregroundColor(Color(red: 209/255, green: 209/255, blue: 218/255))
             }
             .tabItem {
                 Image(systemName: "mic.circle.fill")
