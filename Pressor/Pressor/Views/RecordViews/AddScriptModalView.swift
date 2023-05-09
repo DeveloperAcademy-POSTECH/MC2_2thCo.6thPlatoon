6c84c42e93fc7690420372a317414be1b8893bf5
hajime <impapa68@gmail.com>
2023-05-09 09:34:39 +0900

    [Add/#24] AddScriptModalView 파일 추가

---
+++ Pressor/Pressor/Views/RecordViews/AddScriptModalView.swift
@@ -0,0 +1,20 @@
+//
+//  AddScriptModalView.swift
+//  Pressor
+//
+//  Created by Ha Jong Myeong on 2023/05/09.
+//
+
+import SwiftUI
+
+struct AddScriptModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddScriptModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddScriptModalView()
+    }
+}
