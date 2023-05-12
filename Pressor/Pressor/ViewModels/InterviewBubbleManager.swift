9d2f2542dbba85f59c6dcb4af3cf2009628e06fb
valselee <sollleky72@gmail.com>
2023-05-12 23:27:30 +0900

    [Feat/#33] 각 Record 를 관리하는 Interview의 로직 구현, interview를 init 시점에 받고 해당 interview가 갖는 record를 재생, 정지하는 로직을 갖는다

--- Pressor/Pressor/ViewModels/InterviewBubbleManager.swift
+++ Pressor/Pressor/ViewModels/InterviewBubbleManager.swift
@@ -6,15 +6,66 @@
 //
 
 import SwiftUI
+import AVFoundation
 
-struct InterviewBubbleManager: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+final class InterviewBubbleManager: NSObject, ObservableObject, AVAudioPlayerDelegate {
+    @Published var currentInterview: Interview?
+    private var audioPlayer: AVAudioPlayer = .init()
+    
+    // MARK: LIFECYCLE
+    init(
+        currentInterview: Interview? = nil
+    ) {
+        self.currentInterview = currentInterview
     }
-}
-
-struct InterviewBubbleManager_Previews: PreviewProvider {
-    static var previews: some View {
-        InterviewBubbleManager()
+    
+    // MARK: - Bubble Audio Control
+    public func startPlayingRecordVoice(
+        url: URL,
+        isPlaying: Binding<Bool>
+    ) {
+        let playSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try playSession.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        } catch {
+            print("Playing failed in Device")
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+            isPlaying.wrappedValue.toggle()
+        } catch {
+            print("Playing Failed")
+        }
+    }
+    
+    public func stopPlayingRecordVoice(isPlaying: Binding<Bool>) {
+        audioPlayer.stop()
+        isPlaying.wrappedValue.toggle()
+    }
+    
+    // TODO: PLAY ALL RECORD WITH
+    public func playAllRecord(isPlaying: Binding<Bool>, inList urls: [URL]) {
+        let playSession = AVAudioSession.sharedInstance()
+        var idx = 0
+        
+        do {
+            try playSession.overrideOutputAudioPort(AVAudioSession.PortOverride.speaker)
+        } catch {
+            print("Playing failed in Device")
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: urls[idx])
+            audioPlayer.delegate = self
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        } catch {
+            print("Playing Failed")
+        }
     }
 }
