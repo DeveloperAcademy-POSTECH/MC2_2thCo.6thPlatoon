2d87dd2abcd63d82464fe1d28e432b506ab06a8d
hajime <impapa68@gmail.com>
2023-05-12 22:01:30 +0900

    [Fix/#48] Script 관련 TestView 주석처리(추후 삭제 예정)

--- Pressor/Pressor/Views/TestViews/EditInterviewScriptTestView.swift
+++ Pressor/Pressor/Views/TestViews/EditInterviewScriptTestView.swift
@@ -1,46 +1,46 @@
+////
+////  EditInterviewScriptTestView.swift
+////  Pressor
+////
+////  Created by Ha Jong Myeong on 2023/05/10.
+////
 //
-//  EditInterviewScriptTestView.swift
-//  Pressor
+//import SwiftUI
 //
-//  Created by Ha Jong Myeong on 2023/05/10.
+//struct EditInterviewScriptTestView: View {
+//    @ObservedObject var viewModel: InterviewViewModel
+//    @Environment(\.presentationMode) var presentationMode
+//    @State private var title: String
+//    @State private var content: String
+//    let index: Int
 //
-
-import SwiftUI
-
-struct EditInterviewScriptTestView: View {
-    @ObservedObject var viewModel: InterviewViewModel
-    @Environment(\.presentationMode) var presentationMode
-    @State private var title: String
-    @State private var content: String
-    let index: Int
-    
-    init(viewModel: InterviewViewModel, index: Int, updatedTitle: String, updatedContent: String) {
-        self.viewModel = viewModel
-        self._title = State(wrappedValue: updatedTitle)
-        self._content = State(wrappedValue: updatedContent)
-        self.index = index
-    }
-    
-    var body: some View {
-        VStack {
-            TextField("Enter title", text: $title)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            TextField("Enter content", text: $content)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            Button(action: {
-                viewModel.updateScript(title: title, content: content, atIndex: index)
-                        presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("Update Interview Script")
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding()
-                    .background(Color.green)
-                    .cornerRadius(8)
-            }
-            Spacer()
-        }
-        .padding()
-        .navigationTitle("Edit Interview Script")
-    }
-}
+//    init(viewModel: InterviewViewModel, index: Int, updatedTitle: String, updatedContent: String) {
+//        self.viewModel = viewModel
+//        self._title = State(wrappedValue: updatedTitle)
+//        self._content = State(wrappedValue: updatedContent)
+//        self.index = index
+//    }
+//    
+//    var body: some View {
+//        VStack {
+//            TextField("Enter title", text: $title)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Enter content", text: $content)
+//                .textFieldStyle(RoundedBorderTextFieldStyle())
+//            Button(action: {
+//                viewModel.updateScript(title: title, content: content, atIndex: index)
+//                        presentationMode.wrappedValue.dismiss()
+//            }) {
+//                Text("Update Interview Script")
+//                    .bold()
+//                    .foregroundColor(.white)
+//                    .padding()
+//                    .background(Color.green)
+//                    .cornerRadius(8)
+//            }
+//            Spacer()
+//        }
+//        .padding()
+//        .navigationTitle("Edit Interview Script")
+//    }
+//}
