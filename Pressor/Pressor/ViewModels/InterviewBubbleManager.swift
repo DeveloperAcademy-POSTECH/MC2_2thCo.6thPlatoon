01ff217969f601171e0ac2f2ab0fc4db8af49f33
valselee <sollleky72@gmail.com>
2023-05-12 22:52:32 +0900

    [Add] InterviewBubble 을 관리하는 Manager 파일 추가

---
+++ Pressor/Pressor/ViewModels/InterviewBubbleManager.swift
@@ -0,0 +1,20 @@
+//
+//  InterviewBubbleManager.swift
+//  Pressor
+//
+//  Created by Celan on 2023/05/12.
+//
+
+import SwiftUI
+
+struct InterviewBubbleManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InterviewBubbleManager_Previews: PreviewProvider {
+    static var previews: some View {
+        InterviewBubbleManager()
+    }
+}
