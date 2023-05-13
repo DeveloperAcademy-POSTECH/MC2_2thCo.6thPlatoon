95b43592407314df114b28f60483427c2eb58faa
valselee <sollleky72@gmail.com>
2023-05-13 22:47:58 +0900

    [Feat/#33] 인터뷰 상세 내용을 수정하는 뷰를 재활용, 이전 뷰에서 바인딩된 조건 값에 따라 분기처리하여 수정과 새로운 인터뷰 내용 추가 로직을 태움, 인터뷰의 정보 수정, 인터뷰 내용 수정 기능 구현 완료

--- Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailChatEditModalView.swift
+++ Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailChatEditModalView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct InterviewDetailChatEditModalView: View {
-//    @ObservedObject var recordViewModel: RecordViewModel
+    @ObservedObject var interviewBubbleManager: InterviewBubbleManager
+    @State var record: Record
     @State private var interviewDescription: String = ""
     @Binding var isInterviewDetailChatEditModalViewDisplayed: Bool
     @FocusState var isTextEditorDisplayed: Bool
@@ -18,6 +19,8 @@ struct InterviewDetailChatEditModalView: View {
         NavigationView {
             Form {
                 TextEditor(text: $interviewDescription)
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
                     .frame(
                         minHeight: UIScreen.main.bounds.height * 0.5,
                         maxHeight: .infinity
@@ -44,11 +47,15 @@ struct InterviewDetailChatEditModalView: View {
                         hideKeyboard()
                         dismiss()
                         // TODO: RECORD TEXT UPDATE HERE
+                        interviewBubbleManager.currentInterview.recordSTT[record.transcriptIndex] = interviewDescription
                     } label: {
                         Text("완료")
                     }
                 }
             }
+        }
+        .onAppear {
+            self.interviewDescription =  interviewBubbleManager.currentInterview.recordSTT[record.transcriptIndex]
         }
         .onTapGesture {
             hideKeyboard()
--- Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailEditModalView.swift
+++ Pressor/Pressor/Views/InterviewViews/InterviewModals/InterviewDetailEditModalView.swift
@@ -8,37 +8,60 @@
 import SwiftUI
 
 struct InterviewDetailEditModalView: View {
-    @ObservedObject var vm: VoiceViewModel
+    @ObservedObject var interviewBubbleManager: InterviewBubbleManager
     @EnvironmentObject var routingManager: RoutingManager
-    @State private var interviewTitle: String = Constants.NO_NAME_INTERVIEW
-    @State private var intervieweeName: String = ""
-    @State private var intervieweeEmail: String = ""
-    @State private var intervieweePhoneNumber: String = ""
     @Environment(\.dismiss) var dismiss
+    @Binding var isDetailChanging: Bool
+    
+    private var isNotRequestedInfoAllSubmitted: Bool {
+        interviewBubbleManager.currentInterview.details.userName.isEmpty || interviewBubbleManager.currentInterview.details.interviewTitle.isEmpty
+    }
     
     var body: some View {
-        
         Form {
             Section {
-                TextField("인터뷰 제목", text: $interviewTitle)
+                TextField(
+                    "인터뷰 제목",
+                    text: $interviewBubbleManager.currentInterview.details.interviewTitle
+                )
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
                     .overlay(alignment: .trailing) {
                         Image(systemName: "asterisk")
                             .font(.caption)
-                            .foregroundColor(.PressorRed)
+                            .foregroundColor(Color.pressorRed)
                     }
             } header: {
                 Text("인터뷰 제목")
             }
             
             Section {
-                TextField("이름", text: $intervieweeName)
+                TextField(
+                    "이름",
+                    text: $interviewBubbleManager.currentInterview.details.userName
+                )
+                    .autocorrectionDisabled()
+                    .textInputAutocapitalization(.never)
                     .overlay(alignment: .trailing) {
                         Image(systemName: "asterisk")
                             .font(.caption)
-                            .foregroundColor(.PressorRed)
+                            .foregroundColor(.pressorRed)
                     }
-                TextField("이메일", text: $intervieweeEmail)
-                TextField("전화번호", text: $intervieweePhoneNumber)
+                
+                TextField(
+                    "이메일",
+                    text: $interviewBubbleManager.currentInterview.details.userEmail
+                )
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                
+                TextField(
+                    "전화번호",
+                    text: $interviewBubbleManager.currentInterview.details.userPhoneNumber
+                )
+                .autocorrectionDisabled()
+                .textInputAutocapitalization(.never)
+                
             } header: {
                 Text("대상자 정보")
             } footer: {
@@ -49,34 +72,40 @@ struct InterviewDetailEditModalView: View {
                 }
             }
         }
-        .navigationTitle("인터뷰 정보")
+        .navigationTitle(
+            isDetailChanging
+            ? "인터뷰 수정하기"
+            : "인터뷰 정보"
+        )
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
-                Button {
-                    // TODO: COREDATA CREATE METHOD HERE
-                    dismiss()
-                    routingManager.currentTab = Constants.INTERVIEW_TAB_ID
-                } label: {
-                    Text("완료")
+                if isDetailChanging {
+                    Button {
+                        // 재할당으로 업데이트
+                        interviewBubbleManager.currentInterview = self.interviewBubbleManager.currentInterview
+                        dismiss()
+                    } label: {
+                        Text("완료")
+                    }
+                } else {
+                    NavigationLink {
+                        InterviewDetailView(
+                            interviewBubbleManager: interviewBubbleManager
+                        )
+                    } label: {
+                        Text("완료")
+                    }
+                    // 인터뷰 참여자의 이름이 비어있으면 '완료' 버튼은 비활성화 된다.
+                    .disabled(isNotRequestedInfoAllSubmitted)
                 }
-                // 인터뷰 참여자의 이름이 비어있으면 '완료' 버튼은 비활성화 된다.
-                .disabled(intervieweeName.isEmpty && interviewTitle.isEmpty)
             }
         }
-        
         .onTapGesture {
             hideKeyboard()
         }
         .onDisappear {
             hideKeyboard()
         }
-    }
-}
-
-struct PreviewT: PreviewProvider {
-    static var previews: some View {
-        InterviewDetailEditModalView(vm: VoiceViewModel(
-            interview: Interview(details: InterviewDetail(interviewTitle: "", userName: "", userEmail: "", userPhoneNumber: "", date: Date(), playTime: ""), records: [], recordSTT: [], script: .init(scriptTitle: "", scriptContent: ""))))
     }
 }
