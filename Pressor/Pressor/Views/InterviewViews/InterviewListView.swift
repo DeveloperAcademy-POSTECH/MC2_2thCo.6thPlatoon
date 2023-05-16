7e2c04a1a624ba9f0021a1ae11d69b496de5d553
valselee <sollleky72@gmail.com>
2023-05-16 21:27:48 +0900

    [Feat/#71] InterviewListView UI 및 remove 구현, safe subscript를 활 용하여 뷰 데이터 흐름 안정성 확보, List 대신 ForEach가 idx로 배열을 순회하여 하위 뷰에서 특정 인터뷰 접근을 용이하게 하고 remove, update 등의 로직 구현의 편의성 확보

--- Pressor/Pressor/Views/InterviewViews/InterviewListView.swift
+++ Pressor/Pressor/Views/InterviewViews/InterviewListView.swift
@@ -3,182 +3,87 @@
 //  Pressor
 //
 //  Created by sup on 2023/05/03.
-//
+//  Refactored by Celan on 2023/05/15.
 
 import SwiftUI
 
-//MARK: - interview에서 해당 구조체가 넘어온다고 가정
-// TODO: - 추후에 실제 데이터로 바꿔요
-struct dummyDataa {
-    let id = UUID().uuidString
-    let title: String
-    let date : Date!
-    let interviewTime: String
-}
-
-
 struct InterviewListView: View {
-
-
-    // 위의 Todo의 실제데이터가 들어올 내용이 items입니다.
-    @State var items = [
-        dummyDataa(title: "team1 interview Banana",
-                   date:Calendar.current.date(from: DateComponents(year: 2021, month: 7, day: 1)),
-                   interviewTime: "07:37") ,
-        dummyDataa(title: "team2 interview Banana Melon",
-                   date: Calendar.current.date(from: DateComponents(year: 2022, month: 7, day: 1)),
-                   interviewTime: "32:37"),
-        dummyDataa(title: "team3 interview Melon",
-                   date: Calendar.current.date(from: DateComponents(year: 2023, month: 7, day: 1)),
-                   interviewTime: "12:37")
-
-    ]
-
-    @State var selectedRows = Set<String>()
-
-    @State var isEditing = false
-    @State var showAlert = false
-
-    @State var navigationTitleString = ""
-    @State var searchText = ""
-
-    var searchResults : [dummyDataa] {
-        if searchText.isEmpty {
-            return items
-        } else {
-            return items.filter { $0.title.contains(searchText) }
-        }
-    }
+    @EnvironmentObject var interviewListViewModel: InterviewListViewModel
+    @StateObject var interviewBubbleManager: InterviewBubbleManager = InterviewBubbleManager()
+    @State private var selectedRows = Set<String>()
+    @State private var isEditing = false
+    @State private var showAlert = false
+    @State private var navigationTitleString = ""
+    @State private var searchText = ""
     
     var body: some View {
-
-
         NavigationView {
-
-            VStack{
-                List(selection: $selectedRows) {
-                    ForEach(searchResults, id: \.id) {
-                        one in
-                        VStack(alignment: .leading) {
-                            Text(one.title)
-                                .font(.body)
-
-                            Text(one.date, style: .date)
-                                .font(.caption)
-                                .foregroundColor(.secondary)
-                        }
-                        .listRowBackground(Color(UIColor.systemGray6))
-                        
-                    }
-                    
-                    
-
-                }
-                // NavigationView 전체 색상 적용
-                .scrollContentBackground(.hidden) // NavigationView Background적용을 위한 세팅
-                .background(Color.white) // NavigationView 전체 색상
-                .navigationTitle(isEditing ? "\(selectedRows.count)개가 선택됨" : "인터뷰")
-                .navigationBarTitleDisplayMode(.inline)
-
-
-                .toolbar {
-                    if isEditing {
-                        ToolbarItem(placement: .navigationBarLeading) {
-                            Button {
-                                isEditing.toggle()
-                            } label: {
-                                Text("취소")
-                                    .foregroundColor(Color.orange)
+            List(selection: $selectedRows) {
+                ForEach(
+                    0 ..< interviewListViewModel.interviewList.count,
+                    id: \.self
+                ) {  index in
+                    if
+                        let interviewDetail = interviewListViewModel.getEachInterviewDetail(idx: index) {
+                        NavigationLink {
+                            VStack {
+                                InterviewDetailView(
+                                    interviewBubbleManager: interviewBubbleManager,
+                                    interviewIndex: index
+                                )
                             }
-
-                        }
-                    }
-
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            
-                            if !isEditing {
-                                isEditing.toggle()
-                            } else if isEditing  {
-                                showAlert.toggle()
-                            }
-
-
                         } label: {
-                            Text(isEditing ? "삭제" : "편집" )
-                                .foregroundColor(Color.orange)
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(interviewDetail.interviewTitle)
+                                        .font(.headline)
+                                        .foregroundColor(.primary)
+                                    
+                                    Text(interviewDetail.date.toString(dateFormat: "yyyy. MM. dd. a HH:mm"))
+                                        .font(.subheadline)
+                                        .foregroundColor(Color(.systemGray))
+                                }
+                                
+                                Spacer()
+                                
+                                Text(interviewDetail.playTime)
+                                    .font(.headline)
+                                    .foregroundColor(Color.pressorSystemGray_dark)
+                                
+                            }
                         }
-                        
-                        
-
+                        .listRowBackground(Color.BackgroundGray_Light)
+                    } else {
+                        Text("NO INTERVIEWS TO SHOW")
                     }
-
-
                 }
-
-
-                //편집 클릭시 멀티셀렉션 돌아가게 해주는 내용
-                .environment(\.editMode, .constant(self.isEditing ? .active : .inactive))
-                .animation(.default, value: isEditing)
-
-
-                ForEach(items, id: \.id) {
-                    one in
-                    VStack(alignment: .leading) {
-                        Text(one.title)
-                            .font(.body)
-
-                        Text(one.date, style: .date)
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-
-                    }
+                .onDelete { indexSet in
+                    delete(at: indexSet)
                 }
             }
+            // NavigationView Background적용을 위한 세팅
+            .scrollContentBackground(.hidden)
+            .background(Color.white) // NavigationView 전체 색상
+            .navigationTitle("인터뷰")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                EditButton()
+            }
+            //편집 클릭시 멀티셀렉션 돌아가게 해주는 내용
+            .animation(.default, value: isEditing)
             .searchable(text: $searchText, prompt: "Interview 검색")
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text("인터뷰 삭제"),
-                      message: Text("\(selectedRows.count)개의 인터뷰가 삭제됩니다"),
-                      primaryButton: .destructive(Text("삭제"),
-                                                  action: {
-                    delete()
-                    isEditing.toggle()
-                    
-                } ) ,
-                      secondaryButton: .cancel(Text("취소"))
-                )
-            }
-            
-
-
         }
-
-
-
     }
 
-
-    func swipeDelete(indexSet: IndexSet) {
-        items.remove(atOffsets: indexSet)
+    private func delete(at offsets: IndexSet) {
+        interviewListViewModel.interviewList.remove(atOffsets: offsets)
     }
-    
-    func delete() {
-        print("반복")
-        for item in selectedRows {
-            print(item)
-            if let index = items.lastIndex(where: { $0.id == item }) {
-                items.remove(at: index)
-                
-            }
-        }
-
-    }
-
 }
 
 struct InterviewListView_Previews: PreviewProvider {
     static var previews: some View {
         InterviewListView()
+            .environmentObject(InterviewListViewModel())
     }
 }
 
