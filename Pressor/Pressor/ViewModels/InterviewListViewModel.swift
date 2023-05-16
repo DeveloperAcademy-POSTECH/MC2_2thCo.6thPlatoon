9be808bd0bce1d90a54140baed5acd07b64d1fed
valselee <sollleky72@gmail.com>
2023-05-16 21:32:12 +0900

    [Feat/#71] 리스트를 안전하게 조회할 수 있도록 list의 갯수를 기준으로 get 하는 로직 추가, 옵셔널 흐름 제어로 뷰 안정성 확보

--- Pressor/Pressor/ViewModels/InterviewListViewModel.swift
+++ Pressor/Pressor/ViewModels/InterviewListViewModel.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 final class InterviewListViewModel: ObservableObject {
     @AppStorage(Constants.USERDEFAULTS_INTERVIEWS_KEY) var interviewList: [Interview] = []
+    @Published var currentInterview: Interview = .getDummyInterview()
     
-    private let decoder = JSONDecoder()
-    private let encoder = JSONEncoder()
+    public func getEachInterview(idx: Int) -> Interview? {
+        if interviewList.count > 0 {
+            return self.interviewList[idx]
+        } else {
+            return nil
+        }
+    }
     
+    public func getEachInterviewDetail(idx: Int) -> InterviewDetail? {
+        if interviewList.count > 0 {
+            return self.interviewList[idx].details
+        } else {
+            return nil
+        }
+    }
+    
+    public func getEachInterviewRecordList(idx: Int) -> [Record]? {
+        if interviewList.count > 0 {
+            return self.interviewList[idx].records
+        } else {
+            return nil
+        }
+    }
 }
