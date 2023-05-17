4fba1de52a9627b235c1639c2a3b7f3e771ab9b2
valselee <sollleky72@gmail.com>
2023-05-17 20:30:15 +0900

    [Feat/#77] TextField 오작동 제어를 위해 ViewModel이 Published 된 텍스트필드 바인딩을 보유, 인터뷰의 인스턴스를 받아와서 처리하는 Published 생성

--- Pressor/Pressor/ViewModels/InterviewListViewModel.swift
+++ Pressor/Pressor/ViewModels/InterviewListViewModel.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import Combine
 
 final class InterviewListViewModel: ObservableObject {
-    @AppStorage(Constants.USERDEFAULTS_INTERVIEWS_KEY) var interviewList: [Interview] = []
-    @Published var currentInterview: Interview = .getDummyInterview()
+    @AppStorage(Constants.USERDEFAULTS_INTERVIEWS_KEY)
+    var interviewList: [Interview] = []
+    
+    @Published var completedInterview: Interview = .getInitInterview()
+    @Published var isUpdatingDetails: Bool = false
+    
+    @Published var interviewTitle: String = ""
+    @Published var userName: String = ""
+    @Published var userEmail: String = ""
+    @Published var userPhoneNumber: String = ""
     
     public func getEachInterview(idx: Int) -> Interview? {
         if interviewList.count > 0 {
-            return self.interviewList[idx]
+            return self.interviewList[safe: idx]
         } else {
             return nil
         }
@@ -21,7 +30,7 @@ final class InterviewListViewModel: ObservableObject {
     
     public func getEachInterviewDetail(idx: Int) -> InterviewDetail? {
         if interviewList.count > 0 {
-            return self.interviewList[idx].details
+            return self.interviewList[safe: idx]?.details
         } else {
             return nil
         }
@@ -29,9 +38,31 @@ final class InterviewListViewModel: ObservableObject {
     
     public func getEachInterviewRecordList(idx: Int) -> [Record]? {
         if interviewList.count > 0 {
-            return self.interviewList[idx].records
+            return self.interviewList[safe: idx]?.records
         } else {
             return nil
         }
+    }
+    
+    public func updateInterviewDetails() {
+        self.completedInterview.details.interviewTitle = interviewTitle
+        self.completedInterview.details.userName = userName
+        self.completedInterview.details.userEmail = userEmail
+        self.completedInterview.details.userPhoneNumber = userPhoneNumber
+        initState()
+    }
+    
+    public func updateState(with interview: Interview) {
+        self.interviewTitle = interview.details.interviewTitle
+        self.userName = interview.details.userName
+        self.userEmail = interview.details.userEmail
+        self.userPhoneNumber = interview.details.userPhoneNumber
+    }
+    
+    private func initState() {
+        interviewTitle = ""
+        userName = ""
+        userEmail = ""
+        userPhoneNumber = ""
     }
 }
