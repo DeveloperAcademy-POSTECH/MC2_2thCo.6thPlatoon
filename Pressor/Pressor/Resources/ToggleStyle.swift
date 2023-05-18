73c5c4f914eabe012a666c851c2df8c0e225dfaa
valselee <sollleky72@gmail.com>
2023-05-18 14:08:50 +0900

    [Add/#80] ToggleStyle > Custom Toggle 파일 생성

---
+++ Pressor/Pressor/Resources/ToggleStyle.swift
@@ -0,0 +1,20 @@
+//
+//  ToggleStyle.swift
+//  Pressor
+//
+//  Created by Celan on 2023/05/18.
+//
+
+import SwiftUI
+
+struct ToggleStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ToggleStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleStyle()
+    }
+}
